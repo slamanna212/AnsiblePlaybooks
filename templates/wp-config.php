cbadd513d164c38768089d25d3740db44edf63b7
Slamanna212 <slamanna212@gmail.com>
2021-02-07 15:25:55 -0500

    fix var names

--- templates/wp-config.php
+++ templates/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '{{ mysql_db }}' );
+define( 'DB_NAME', '{{ sqlname }}' );
 
 /** MySQL database username */
-define( 'DB_USER', '{{ mysql_user }}' );
+define( 'DB_USER', '{{ sqluser }}' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '{{ mysql_password }}' );
+define( 'DB_PASSWORD', '{{ sqlpass }}' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
